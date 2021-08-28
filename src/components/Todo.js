5cdd4e1b39e45c5130ff86037294a9da488cec34
Jack Rigan <riganjack40@gmail.com>
2021-08-27 18:55:11 -0600

    created completeHandler to update the state of a todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,19 +6,31 @@ import {BsFillTrash2Fill} from 'react-icons/bs';
 
 
 function Todo( { text,todo, todos, setTodos } ) {
-
 // Events
 const deleteHandler = () => {
 //Filter the state and find element that matches the element i clicked on
 setTodos(todos.filter((element) => element.id !== todos.id));
-console.log(todo)
 
+console.log(todo)
 }
+
+const completeHandler = () => {
+    setTodos(todos.map((item) => {
+        if(item.id === todo.id) {
+            return {
+                ...item, completed: !item.completed
+            }
+        }
+        //incase it does not match just return whatever it wants
+        return item;
+    }))
+}
+
 
     return (
         <div>
         <li className='todo-item'>{text}</li>
-        <button><AiFillCheckCircle/></button>
+        <button onClick={completeHandler}><AiFillCheckCircle/></button>
         <button onClick={deleteHandler}><BsFillTrash2Fill/></button>
         </div>
     )
