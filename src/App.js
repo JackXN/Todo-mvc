eb89b732a9b7be19b37a48c036acb07d17155b4d
Jack Rigan <riganjack40@gmail.com>
2021-08-27 19:06:16 -0600

    initialized state for item status

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import React, {useState} from 'react';
 import Todo from './components/Todo'
 
 function App() {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState('');
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+
 
   return (
     <>
     <h1>Todo App
     </h1>
     {/* Setting the prop value */}
-<TodoForm setInput={setInput} setTodos={setTodos} todos={todos} input={input}/>
+<TodoForm setInput={setInput} setTodos={setTodos} todos={todos} input={input} setStatus={setStatus}/>
 <List todos={todos} setTodos={setTodos}/>
 
     </>
